966cb7872b380ae2b727c391b56ad6726ba8dde7
Mayya Bareeva <mayya.bareeva@zolar.de>
2020-03-08 23:56:08 +0100

    the random emails are added to the txt file

--- src/main/java/mailingtask/Main.java
+++ src/main/java/mailingtask/Main.java
@@ -4,9 +4,12 @@ import javax.mail.*;
 import javax.mail.internet.*;
 
 public class Main {
+
+	private static final String initialFileName = "emailAdresses.txt";
+	
 	public static void main(String[] args) throws InterruptedException {
 		EmailSender sender = new EmailSender();
-		sender.setProperties("recipient@gmail.com, test@gmail.com");
+		sender.setProperties(initialFileName);
 		sender.setEmailsAmount(100);
 		new Thread(sender).start();
 	}
